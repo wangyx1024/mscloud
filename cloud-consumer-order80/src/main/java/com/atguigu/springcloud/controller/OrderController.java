96e754645edfffefcad306d6f90ccd1b645c8c44
王雁翔 <1668171078@qq.com>
2021-05-29 15:12:46 +0800

    引入单机eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.CommonResult;
 import com.atguigu.springcloud.entities.Payment;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,10 +19,10 @@ import javax.annotation.Resource;
 @Slf4j
 public class OrderController {
 
-    private static final String PAYMENT_DOMAIN = "http://localhost:8001";
-
     @Resource
     private RestTemplate restTemplate;
+
+    public static final String PAYMENT_DOMAIN = "http://CLOUD-PAYMENT-SERVICE";
 
     /**
      * 模拟用户端，用浏览器访问get方便
